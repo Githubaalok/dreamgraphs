9c45c0884e7bf29b0760bf00aa5fe06e734a9d05
Aalok <aalokmali.mits@gmail.com>
2017-05-06 12:52:21 +0530

    06.05.2017 12.52
    ddfgdf

--- www/js/app.js
+++ www/js/app.js
@@ -20,18 +20,14 @@ angular.module('starter', ['ionic', 'starter.controllers','ionic-datepicker','ng
       StatusBar.styleDefault();
     }
 	// One Signal Code
-			var notificationOpenedCallback = function(jsonData) {
-		  //console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-		  var data = { origin:jsonData.additionalData.origin};
-		  $state.go('app.wall',data);
-		 };
-
-		 window.plugins.OneSignal.init("6abc3b85-b260-4a6c-a1b6-153a07812d3d",
-				 {googleProjectNumber: "823030988590"},
-				 notificationOpenedCallback);
-		  
-		 // Show an alert box if a notification comes in when the user is in your app.
-		 window.plugins.OneSignal.enableInAppAlertNotification(true);
+		var notificationOpenedCallback = function(jsonData) {
+			console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+			$state.go("app.wall");
+		};
+		window.plugins.OneSignal
+		.startInit("6abc3b85-b260-4a6c-a1b6-153a07812d3d")
+		.handleNotificationOpened(notificationOpenedCallback)
+		.endInit();
 	// One Signal Code End
   });
 })
